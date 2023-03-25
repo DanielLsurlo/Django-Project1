05e25c80cb2acc40202d2a22c23b80c44d5fe338
Daniel Surlo <daniel.surlo0@gmail.com>
2023-03-25 07:32:15 -0300

    add recipe-view template

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path  # type:ignore
 
-from recipes.views import home
+from . import views
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render  # type: ignore
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Primeiro Nome'})
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Primeiro Nome'})
