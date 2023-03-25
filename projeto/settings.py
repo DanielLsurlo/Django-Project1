f76120b29f6fee88d8654aec5938d56cb0780732
Daniel Surlo <daniel.surlo0@gmail.com>
2023-03-25 06:42:08 -0300

    STATIC_URLS and STATIC_FILES

--- projeto/settings.py
+++ projeto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5sp6yhhtw-!rtf8*&qzyqorl!x83f5&@4!8d6pwl1su714l$6p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: list = []
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'base_templates'
+            BASE_DIR / 'base_templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -122,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
