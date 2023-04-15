d7be5dfa3313f99e8ba39cc8d0ec1b528727d09b
Daniel Surlo <daniel.surlo0@gmail.com>
2023-04-15 09:26:00 -0300

    forms validation

--- authors/forms.py
+++ authors/forms.py
@@ -1,9 +1,25 @@
+import re
+
 from django import forms  # type: ignore
 from django.contrib.auth.models import User  # type: ignore
+from django.core.exceptions import ValidationError  # type: ignore
 
 
 def add_placeholder(field, placeholder_val):
     field.widget.attrs['placeholder'] = placeholder_val
+
+
+def strong_password(password):
+    regex = re.compile(r'(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='invalid',
+        )
 
 
 class RegisterForm(forms.ModelForm):
@@ -33,8 +49,12 @@ class RegisterForm(forms.ModelForm):
             'at least 8 characters.'
         ),
         label='Password:',
+        widget=forms.PasswordInput(),
+        validators=[strong_password],
 
     )
+
+    email = forms.EmailField()
 
     class Meta:
         model = User
@@ -60,3 +80,34 @@ class RegisterForm(forms.ModelForm):
                 'required': 'This field does not be empty',
             }
         }
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+
+        if 'atenção' in data:
+            raise ValidationError(
+                'Não digite %(value)s no campo password',
+                code='invalid',
+                params={'value': '"atenção"'}
+            )
+
+        return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            password_confirm_error = ValidationError(
+                'Passwords must be equal.',
+                code='invalid'
+            )
+            raise ValidationError({
+                'password': password_confirm_error,
+                'password2': [
+                    password_confirm_error,
+                ],
+            }
+            )
