75914071289db16ab7cacc2243228dfdbb027a4f
Daniel Surlo <daniel.surlo0@gmail.com>
2023-04-18 07:10:19 -0300

    login authenticated

--- authors/views.py
+++ authors/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages  # type: ignore
+from django.contrib.auth import authenticate, login  # type: ignore
 from django.http import Http404  # type: ignore
 from django.shortcuts import redirect, render  # type: ignore
 from django.urls import reverse  # type: ignore
@@ -43,4 +44,24 @@ def login_view(request):
 
 
 def login_create(request):
-    return render(request, 'authors/pages/login.html')
+    if not request.POST:
+        raise Http404()
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticate_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticate_user is not None:
+            messages.success(request, 'You are logged in.')
+            login(request, authenticate_user)
+
+        else:
+            messages.error(request, 'Invalid credentials')
+    else:
+        messages.error(request, 'Invalid username or password')
+
+    return redirect(login_url)
