ebd590158a2f4cea82692ff3529798d70f5b3ca0
Daniel Surlo <daniel.surlo0@gmail.com>
2023-04-19 07:28:09 -0300

    functional tests

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -4,13 +4,6 @@ from recipes.models import Category, Recipe, User
 
 
 class RecipeMixin:
-    ...
-
-
-class RecipeTestBase(TestCase):
-    def setUp(self) -> None:
-        return super().setUp()
-
     def make_category(self, name='Category'):
         return Category.objects.create(name=name)
 
@@ -98,3 +91,19 @@ class RecipeTestBase(TestCase):
             preparation_step_is_html=preparation_step_is_html,
             is_published=is_published,
         )
+
+    def make_recipe_in_batch(self, qtd=10):
+        recipes = []
+        for i in range(qtd):
+            kwargs = {
+                'title': f'Recipe Title {i}',
+                'slug': f'r{i}',
+                'author_data': {'username': f'u{i}'}}
+            recipe = self.make_recipe(**kwargs)
+            recipes.append(recipe)
+        return recipes
+
+
+class RecipeTestBase(TestCase, RecipeMixin):
+    def setUp(self) -> None:
+        return super().setUp()
