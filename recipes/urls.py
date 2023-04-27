adae7be69703a0b8da7b135dc633e9144b27b8e8
Daniel Surlo <daniel.surlo0@gmail.com>
2023-04-27 07:05:38 -0300

    refactoring home recipe view

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,3 +1,4 @@
+# type: ignore
 from django.urls import path  # type:ignore
 
 from . import views
@@ -5,7 +6,7 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.RecipeListViewBase.as_view(), name="home"),
     path('recipes/search/', views.search, name="search"),
     path('recipes/category/<int:category_id>/',
          views.category, name="category"),
