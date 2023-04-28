cf50bc3b1449313dfe6d17dfafc93e7e722d68d2
Daniel Surlo <daniel.surlo0@gmail.com>
2023-04-28 06:05:39 -0300

    Correct test and refactor detail page to CBV

--- recipes/views.py
+++ recipes/views.py
@@ -3,8 +3,8 @@ import os
 
 from django.db.models import Q
 from django.http.response import Http404
-from django.shortcuts import get_list_or_404, get_object_or_404, render
-from django.views.generic import ListView
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import DetailView, ListView
 
 from recipes.models import Recipe
 from utils.pagination import make_pagination
@@ -54,6 +54,9 @@ class RecipeListViewCategory(RecipeListViewBase):
         qs = qs.filter(
             category__id=self.kwargs.get('category_id'),
         )
+
+        if not qs:
+            raise Http404
         return qs
 
     def get_context_data(self, *args, **kwargs):
@@ -70,6 +73,9 @@ class RecipeListViewSearch(RecipeListViewBase):
 
     def get_queryset(self, *args, **kwargs):
         search_term = self.request.GET.get('q', '')
+
+        if not search_term:
+            raise Http404()
 
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(
@@ -93,33 +99,19 @@ class RecipeListViewSearch(RecipeListViewBase):
         return ctx
 
 
-def home(request):
-    recipes = Recipe.objects.filter(
-        is_published=True
-    ).order_by('-id')
+class RecipeDetail(DetailView):
+    model = Recipe
+    context_object_name = 'recipe'
+    template_name = 'recipes/pages/recipe-view.html'
 
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
 
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': page_obj,
-        'pagination_range': pagination_range,
-    })
+        ctx.update({
+            'is_detail_page': True
+        })
 
-
-def category(request, category_id):
-    recipes = get_list_or_404(
-        Recipe.objects.filter(
-            category__id=category_id,
-            is_published=True,
-        ).order_by('-id'))
-
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
-    return render(request, 'recipes/pages/category.html', context={
-        'recipes': page_obj,
-        'pagination_range': pagination_range,
-        'title': f'{recipes[0].category.name} - Category | ',
-    })
+        return ctx
 
 
 def recipe(request, id):
@@ -128,31 +120,4 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
-    })
-
-
-def search(request):
-    search_term = request.GET.get('q', '').strip()
-
-    if not search_term:
-        raise Http404()
-
-    # Making complex lookups with objects Q:
-    # https://docs.djangoproject.com/pt-br/3.2/topics/db/queries/#complex-lookups-with-q-objects
-    recipes = Recipe.objects.filter(
-        Q(
-            Q(title__icontains=search_term) | Q(
-                description__icontains=search_term),
-        ),
-        is_published=True
-    ).order_by('-id')
-
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
-    return render(request, 'recipes/pages/search.html', {
-        'page_title': f'Search for "{search_term}" |',
-        'search_term': search_term,
-        'recipes': page_obj,
-        'pagination_range': pagination_range,
-        'additional_url_query': f'&q={search_term}',
     })
